e05fdd3463be5cf348c291adcef24745db963846
Stef <stef@infi.nl>
2024-01-26 16:06:17 +0100

    Day 3 - Part 1

--- main.go
+++ main.go
@@ -3,19 +3,5 @@ package main
 import "log"
 
 func main() {
-	input := getInput()
-
-	for _, line := range input {
-		gameId, err := getGameID(line)
-		if err != nil {
-			log.Println("Error getting game ID", err)
-			return
-		}
-		log.Println(gameId)
-
-		rounds := getRounds(line)
-		calculatePowerOfGame(rounds)
-	}
-
-	log.Println("Total", total)
+	log.Println("Hello World")
 }
