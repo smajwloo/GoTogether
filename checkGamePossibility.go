fb146356439b9667d1cc55fdf5fcc0d5d670fcf5
Stef <stef@infi.nl>
2024-01-26 15:30:42 +0100

    Day 2 - Part 1

--- checkGamePossibility.go
+++ checkGamePossibility.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 )
 
+// TODO: Remove parts that come from part 1, these are not needed anymore
 var colors = map[string]int{
 	"red":   12,
 	"green": 13,
@@ -13,15 +14,16 @@ var colors = map[string]int{
 }
 
 func checkGamePossibility(rounds []string) bool {
+	power := 0
 	for _, round := range rounds {
-		if !checkRoundPossibility(round) {
+		if !checkRoundPossibility(round, power) {
 			return false
 		}
 	}
 	return true
 }
 
-func checkRoundPossibility(round string) bool {
+func checkRoundPossibility(round string, power int) bool {
 	for color, maxValue := range colors {
 		if strings.Contains(round, color) {
 			first := strings.Split(round, " "+color)[0]
@@ -37,8 +39,34 @@ func checkRoundPossibility(round string) bool {
 			if amount > maxValue {
 				return false
 			}
+
+			currentAmounts := getLowerNumber(amount)
+			power = calculatePowerOfCubes(currentAmounts, power)
+			log.Println("Power", power)
 		}
 
 	}
 	return true
+}
+
+func calculatePowerOfCubes(currentAmounts map[string]int, power int) int {
+	for _, amount := range currentAmounts {
+		if power == 0 {
+			power = amount
+		} else {
+			power = power * amount
+		}
+	}
+	return power
+}
+
+func getLowerNumber(currentAmount int) map[string]int {
+	currentAmounts := colors
+	for color, amount := range currentAmounts {
+		if currentAmount < amount {
+			currentAmounts[color] = currentAmount
+			log.Println("Current amount", currentAmount)
+		}
+	}
+	return currentAmounts
 }
